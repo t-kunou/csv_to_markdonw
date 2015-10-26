62a5e78a6739b5f3c60375f14dec65fce8c32075
kunou <t-kunou@esm.co.jp>
2015-10-26 18:25:28 +0900

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to csv_to_markdonw
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/csv_to_markdonw/core_test.clj
@@ -0,0 +1,7 @@
+(ns csv-to-markdonw.core-test
+  (:require [clojure.test :refer :all]
+            [csv-to-markdonw.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
